53814937d29184c77e620139eb79019942ebd6d9
yggdrasil <izamrosiawan13@gmail.com>
2026-07-18 17:11:24 +0700

    refactor: remove unused and deprecated authentication service files

--- scratch/dump_cell_10.py
+++
@@ -1,6 +0,0 @@
-import nbformat
-
-with open(r"c:\Users\LENOVO\Documents\GitHub\olist-brazilian-ecommerce-analytics\notebook.ipynb", encoding='utf-8') as f:
-    nb = nbformat.read(f, as_version=4)
-
-print(nb.cells[10].source)
--- scratch/dump_cells_2.py
+++
@@ -1,9 +0,0 @@
-import nbformat
-
-with open(r"c:\Users\LENOVO\Documents\GitHub\olist-brazilian-ecommerce-analytics\notebook.ipynb", encoding='utf-8') as f:
-    nb = nbformat.read(f, as_version=4)
-
-cells_to_print = [12, 15, 18]
-for idx in cells_to_print:
-    print(f"\n==================== CELL {idx} ====================")
-    print(nb.cells[idx].source)
